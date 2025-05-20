8c888ac762df9463a075135ca35c1d21b20f0d8d
Amil Muhammed Hamza <amilmohd155@gmail.com>
2025-05-20 12:57:54 +0100

    refactor: :arrow_up: Refactor code structure for improved readability and maintainability

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
-}
+//Timer component
+export { default as Timer } from './components/timer';
+export type { TimerProps, TimerMethods } from './components/timer';
+
+//Stopwatch component
+export { default as Stopwatch } from './components/stopwatch';
+export type { StopwatchProps, StopwatchMethods } from './components/stopwatch';
+
+//Clock component
+export { default as Clock } from './components/clock';
+export type { ClockProps } from './components/clock';
